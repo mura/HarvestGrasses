0243ab2ec9d14f37a2cbbfffa3f0c7a3d582ad74
Yohei Murayama <muracchi@gmail.com>
2019-08-24 00:29:03 +0900

    fix crash

--- src/main/java/jp/stoic/minecraft/harvestgrasses/HarvestGrassesMod.java
+++ src/main/java/jp/stoic/minecraft/harvestgrasses/HarvestGrassesMod.java
@@ -1,14 +1,21 @@
 package jp.stoic.minecraft.harvestgrasses;
 
-import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.IForgeRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(HarvestGrassesMod.MODID)
@@ -16,6 +23,9 @@ public class HarvestGrassesMod {
     static final String MODID = "harvestgrassesmod";
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
+    private final static List<String> noDropItemNames = Arrays.asList(
+            "air", "barrier"
+    );
 
     public HarvestGrassesMod() {
         // Register the doClientStuff method for modloading
@@ -23,9 +33,19 @@ public class HarvestGrassesMod {
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
-        ForgeRegistries.ITEMS.getValues().stream()
-                .filter(item -> !Items.AIR.equals(item))
-                .forEach(item -> MinecraftForge.addGrassSeed(new ItemStack(item), 10));
+        IForgeRegistry<Item> itemRegistry = GameRegistry.findRegistry(Item.class);
+        Set<Item> noDropItems = noDropItemNames.stream()
+                .map(ResourceLocation::new)
+                .filter(itemRegistry::containsKey)
+                .map(itemRegistry::getValue)
+                .collect(Collectors.toSet());
+        itemRegistry.getValues().stream()
+                .filter(item -> !noDropItems.contains(item))
+                .forEach(this::addGrassSeed);
         LOGGER.info("It's Harvest Time!");
+    }
+
+    private void addGrassSeed(Item item) {
+        MinecraftForge.addGrassSeed(new ItemStack(item), 10);
     }
 }
